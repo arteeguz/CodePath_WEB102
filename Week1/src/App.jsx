37a2bcee0dba70932c53bc04152dbc4318b8c77c
arteeguz <arteeguz@gmail.com>
2024-02-29 22:01:37 -0500

    Initial commit

---
+++ Week1/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Calendar from './components/Calendar'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Title</h1>
+      <h2>Message</h2>
+      <Calendar />
+
+      
+    </div>
+  )
+}
+
+export default App
