975c46ef3b7f33ba0aea87b48217ec31e891cccc
arteeguz <arteeguz@gmail.com>
2024-03-16 14:46:01 -0400

    Added APIFrom.jsx

---
+++ Labs/Week4/Week4Lab/src/components/APIFrom.jsx
@@ -0,0 +1,33 @@
+const APIForm = ({inputs, handleChange, onSubmit}) => {
+
+
+    return (
+        <div>
+            <h2> Select Your Image Attributes </h2>
+            <form className="form-container">
+                {inputs &&
+                    Object.entries(inputs).map(([category, value], index) => (
+                    <li className="form" key={index}>
+                        <h2>{category} </h2>
+                        <input
+                        type="text"
+                        name={category}
+                        value={value}
+                        placeholder="Input this attribute..."
+                        onChange={handleChange}
+                        className="textbox"
+                        />
+                        <br></br>
+                        <br></br>
+                        <p> {inputsInfo[index]}</p>
+                    </li>
+                    ))}
+                </form>
+                <button type="submit" className="button" onClick={onSubmit}>
+                    Take that Pic!
+                </button>
+        </div>
+    );
+};
+
+export default APIForm;
